32d6f12d89d02e9fcfeedc17d22d3a0e0f12c18a
Damian Monogue <demonnic@gmail.com>
2019-09-29 20:09:09 -0400

    Missed some untracked files in previous commit

---
+++ static/i18n/en.lua
@@ -0,0 +1,10 @@
+return {
+  en = {
+    upload_success = "Package %s uploaded successfully",
+    greeting = "Hello there, %s",
+    login_nav = "Login",
+    register_nav = "Register",
+    index_nav = "Index",
+    packages_nav = "Packages"
+  }
+}

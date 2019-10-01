23c4da7187f47f0de0cacb1f8c504c9ae11592d6
Vadim Peretokin <vperetokin@gmail.com>
2019-10-01 13:37:32 +0200

    Add Russian translation so there's something to work with (#1)
    * Add Russian translation so there's something to work with
    
    * Add new strings for translation

--- static/i18n/ru.lua
+++ static/i18n/ru.lua
@@ -1,5 +1,22 @@
 return {
   ru = {
-    
+    upload_success = "Пакет %s загружен успешно",
+    greeting = "Здраствуйте, %s",
+    login_nav = "Вход",
+    register_nav = "Регистрация",
+    index_nav = "Индекс",
+    packages_nav = "Пакеты",
+    website_name = "Репозиторий Пакетов Mudlet",
+    err_invalid_user = "Ошибка: Неверное имя пользователя или пароль",
+    err_create_user = "Ошибка: не удалось создать пользователя %s",
+    err_user_exists = "Ошибка: Пользователь %s уже существует",
+    username = "имя пользователя",
+    password = "пароль",
+    login = "логин",
+    verify_password = "Проверьте пароль",
+    email_address = "адрес электронной почты",
+    administrator = "администратор",
+    user_created = "Пользователь создан",
+    error_header = "ОШИБКА",
   }
 }

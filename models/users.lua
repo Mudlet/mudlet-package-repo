83a8501c07b119a7ee65c10da7ac1bae694dca15
Edru2 <alyven_87@hotmail.com>
2020-05-21 15:34:02 +0200

    Revert "solves register code problem"
    This reverts commit d6a046e45b7b693e954a617ad9a86813791fcecc.

--- models/users.lua
+++ models/users.lua
@@ -11,7 +11,6 @@ local Users = Model:extend("users", {
 })
 
 local function uuid()
-  math.randomseed(os.clock()*1000000)
   local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
   return string.gsub(template, '[xy]', function (c)
     local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)

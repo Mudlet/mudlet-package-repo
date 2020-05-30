7a1930b08b25bcabf01fdfb1d16af444b460e4e4
Vadim Peretokin <vperetokin@gmail.com>
2020-05-30 09:43:57 +0200

    Remove debug prints

--- controllers/uploadpackage.lua
+++ controllers/uploadpackage.lua
@@ -37,30 +37,25 @@ local function save_file(self)
   -- record our current directory so we can change back to it
   local cwd = lfs.currentdir()
   local file_extension = string.match(self.params.file.filename, ".+%.(.+)")
-  print"1"
+
   -- start in the data directory
   local success, message = lfs.chdir(self.config.data_dir)
   if not success then lfs.chdir(cwd); yield_error("Couldn't enter data directory: "..message) end
-  print"2"
+
 
   -- create username directory if it doesn't exist, validate it's a dir and enter it
   if not isFileOrDir(self.session.name) then
-    print"3"
     local success, message = lfs.mkdir(self.session.name)
     if not success then lfs.chdir(cwd); yield_error("Couldn't create username directory: "..message) end
   end
-  print"4"
-  -- yield_error(isDir(self.session.name), self.i18n("err_save_file"))
   local success, message = lfs.chdir(self.session.name)
   if not success then lfs.chdir(cwd); yield_error("Couldn't enter username directory: "..message) end
-  print"5"
 
   -- and again for the pkg name
   if not isFileOrDir(self.params.name) then
     local success, message = lfs.mkdir(self.params.name)
     if not success then lfs.chdir(cwd); yield_error("Couldn't create package directory: "..message) end
   end
-  -- yield_error(isDir(self.params.name), self.i18n("err_save_file"))
   local success, message = lfs.chdir(self.params.name)
   if not success then lfs.chdir(cwd); yield_error("Couldn't enter package directory: "..message) end
 
@@ -69,7 +64,6 @@ local function save_file(self)
     local success, message = lfs.mkdir(self.params.version)
     if not success then lfs.chdir(cwd); yield_error("Couldn't create package version directory: "..message) end
   end
-  -- yield_error(isDir(self.params.version), self.i18n("err_save_file"))
   local success, message = lfs.chdir(self.params.version)
   if not success then lfs.chdir(cwd); yield_error("Couldn't enter package version directory: "..message) end
 
